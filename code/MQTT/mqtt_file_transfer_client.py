e48c4f3091b750ec7d78b3b126f85ac6eb03413f
Justin Pahl <jppahl@ncsu.edu>
2022-02-09 20:57:56 -0500

    Added queue for processing messages, added back publisher stats file.

--- code/MQTT/mqtt_file_transfer_client.py
+++ code/MQTT/mqtt_file_transfer_client.py
@@ -4,6 +4,7 @@ import os
 import sys
 import math
 import time
+from queue import Queue
 
 
 
@@ -27,11 +28,12 @@ class Client:
         self.client.on_subscribe = self.on_subscribe
         self.client.on_publish = self.on_publish
 
-        # Add some helper flags to client oject
+        # Add some helper parameters to client oject
         self.client.is_connected_flag = False
         self.client.is_subscribed_flag = False
         self.client.publish_is_complete_flag = False
         self.client.num_times_to_loop = self.count
+        self.message_queue = Queue()
 
         try:
             self.client.connect(self.bkr_addr, self.bkr_port)
@@ -70,26 +72,23 @@ class Client:
 
         # Variable Header
         header_size += 2 # Fixed topic length field
-        header_size += len(args.topic) # Topic
+        header_size += len(self.topic) # Topic
         header_size += 2 # Message id field
 
-
         fo_stats.write(str(time.time()) + "," + str(payload_length + header_size) + "\n")
-        fout=open(self.file,"wb")
-        fout.write(msg.payload)
-        fout.close()
+
+        # Write the file later to allow the function to return 
+        self.message_queue.put(msg.payload)
         client.num_times_to_loop -= 1
         print("Received File Copy (%d of %d)" %(self.count - client.num_times_to_loop, self.count))
 
     def publish_data(self, payload):
-        # fo_stats.write(str(time.time())+",\n")
+        fo_stats.write(str(time.time())+",\n")
         self.client.publish_is_complete_flag = False
         self.client.publish(self.topic, payload, self.qos)
 
     def on_publish(self, client,userdata,result):             #create function for callback
         client.publish_is_complete_flag = True
-
-
 
     def disconnect(self):
         self.client.disconnect()
@@ -97,6 +96,8 @@ class Client:
 
     def publish(self):
         # self.client = self.connect()
+        global fo_stats
+        fo_stats = open(self.file + "_" + "publish" + "_qos_" + str(1) + "_stats.csv","w")
         try:
             with open(self.file,"rb") as f:
                 file_size = os.path.getsize(self.file)
@@ -117,7 +118,7 @@ class Client:
     def subscribe(self):
         # self.client = self.connect(self.bkr_addr, self.bkr_port, self.count, "subscriber")
         global fo_stats
-        fo_stats = open("op" + "_" + "subscriber" + "_qos_" + str(1) + "_stats.csv","w")
+        fo_stats = open(self.file + "_" + "subscriber" + "_qos_" + str(1) + "_stats.csv","w")
 
         # Subscribing to topic and waiting for SUBACK
         print("Subscribing to topic: " + self.topic + ", QOS:" + str(self.qos))
@@ -125,21 +126,27 @@ class Client:
         while not self.client.is_subscribed_flag:
             continue
 
-        # Loop until we've received the required number of file copies
-        while self.client.num_times_to_loop:
+        # Loop until we've received and written the required number of file copies
+        while (self.client.num_times_to_loop > 0) or (not self.message_queue.empty()):
+            message_payload = self.message_queue.get()
+            if not message_payload is None:
+                fout=open(self.file,"wb")
+                fout.write(message_payload)
+                fout.close()
             continue
+
         self.disconnect()
 
 
 if __name__ == "__main__":
 
+    if True:
     # use this code block for publish
-    # client = Client('192.168.1.9', 1883, '10MB', 'topic1', 1, 10, 'publisher')
-    # client.connect()
-    # client.publish()
-    # client.disconnect()
-
+     client = Client('localhost', 1883, '10MB', 'topic1', 1, 10, 'publisher')
+     client.connect()
+     client.publish()
+    else: 
     # Use this code block for client
-    # client = Client('192.168.1.9', 1883, 'op', 'topic1', 1, 10, 'subscriber')
-    # client.connect()
-    # client.subscribe()
+     client = Client('localhost', 1883, 'op', 'topic1', 1, 10, 'subscriber')
+     client.connect()
+     client.subscribe()
