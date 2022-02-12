183b4f53809cd6f1d10b39995360939dc78c4c3b
Ashok Kumar Selvam <aselvam@ncsu.edu>
2022-02-12 15:59:52 -0500

    Add properties

--- code/MQTT/main.py
+++ code/MQTT/main.py
@@ -1,13 +1,11 @@
 from mqtt_file_transfer_client import Client
 import argparse
 import os
+import json
 
 SRC_FILE_PATH = '../../DataFiles/'
 DEST_FILE_PATH = 'ReceivedFiles/'
-
 SOURCE_FILES = {'100B': 10000, '10KB': 1000, '1MB': 100, '10MB': 10}
-BROKER_ADDR = 'localhost'
-BROKER_PORT = 1883
 
 def mqtt_sub(qos):
     client = Client(BROKER_ADDR, BROKER_PORT, 'subscriber')
@@ -26,7 +24,16 @@ def mqtt_pub(qos):
         client.publish(SRC_FILE_PATH + f, 'topic1', qos, SOURCE_FILES[f])
     client.disconnect()
 
+def init():
+    f = open('../../properties.json')
+    properties = json.load(f)
+    global BROKER_ADDR
+    BROKER_ADDR = properties['MQTT']['BROKER_ADDR']
+    global BROKER_PORT
+    BROKER_PORT = properties['MQTT']['BROKER_PORT']
+
 if __name__ == "__main__":
+    init()
     parser = argparse.ArgumentParser(description = "MQTT File Transfer Client")
     parser.add_argument("-s", "--subscriber", help="Run Client as Subscriber.", action="store_true")
     parser.add_argument("-p", "--publisher", help="Run Client as Publisher.", action="store_true")
